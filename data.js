a83773d64ce83b6ff923ccf5f6ff5ce9ded01822
smackinnon-uepm <smackinnon@unensayoparami.org>
2026-05-27 20:05:42 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,296 @@
+// Generado: 27/05/2026 15:47 hora Argentina
+window.DASHBOARD_DATA = [
+  {
+    "name": "ARG-ALZ-KARUNA-BMS-ADEPT4",
+    "acs": [
+      {
+        "name": "Ariadna Aixa Yael  Castillo",
+        "fte": 8.0
+      },
+      {
+        "name": "Eliangelys  Camejo",
+        "fte": 6.0
+      },
+      {
+        "name": "Norberto Selasco",
+        "fte": 4.0
+      }
+    ],
+    "total_fte": 18.0,
+    "ingresos_hoy": 5,
+    "backlog": {
+      "Ingreso del Paciente": 2,
+      "En gestión AC": 76,
+      "Derivado": 8,
+      "Consentimiento Firmado": 4,
+      "Visita Programada": 4,
+      "Visita Cumplida": 1,
+      "Pre Derivado": 1
+    },
+    "total_backlog": 96,
+    "gina_hoy": 0,
+    "gina_total": 0
+  },
+  {
+    "name": "ARG-CVOT-AMG-MARITIME196",
+    "acs": [
+      {
+        "name": "Cinthia Nogues",
+        "fte": 8.0
+      },
+      {
+        "name": "Milagros  Tani",
+        "fte": 8.0
+      },
+      {
+        "name": "Roxana Lorena Diaz",
+        "fte": 8.0
+      }
+    ],
+    "total_fte": 24.0,
+    "ingresos_hoy": 13,
+    "backlog": {
+      "En gestión AC": 300,
+      "Ingreso del Paciente": 91,
+      "Contactado por el Investigador": 12,
+      "Visita Programada": 12,
+      "Consentimiento Firmado": 5,
+      "Derivado": 13,
+      "Visita Cumplida": 2
+    },
+    "total_backlog": 435,
+    "gina_hoy": 22,
+    "gina_total": 935
+  },
+  {
+    "name": "ARG-HF-AMG-MARITIME227",
+    "acs": [
+      {
+        "name": "Antonella Mellado",
+        "fte": 8.0
+      },
+      {
+        "name": "Giannina La pietra",
+        "fte": 8.0
+      },
+      {
+        "name": "Agustina Tanca",
+        "fte": 6.0
+      }
+    ],
+    "total_fte": 22.0,
+    "ingresos_hoy": 4,
+    "backlog": {
+      "Ingreso del Paciente": 303,
+      "En gestión AC": 100,
+      "Visita Programada": 1,
+      "Visita Cumplida": 1,
+      "Derivado": 2
+    },
+    "total_backlog": 407,
+    "gina_hoy": 9,
+    "gina_total": 170
+  },
+  {
+    "name": "BRA-ALZ-KARUNA-BMS-ADEPT4",
+    "acs": [
+      {
+        "name": "Maria Fernanda  Barbosa",
+        "fte": 8.0
+      },
+      {
+        "name": "Alessandra D'Amico",
+        "fte": 8.0
+      }
+    ],
+    "total_fte": 16.0,
+    "ingresos_hoy": 8,
+    "backlog": {
+      "En gestión AC": 66,
+      "Visita Cumplida": 4,
+      "Contactado por el Investigador": 6,
+      "Consentimiento Firmado": 3,
+      "Visita Programada": 1
+    },
+    "total_backlog": 80,
+    "gina_hoy": 0,
+    "gina_total": 0
+  },
+  {
+    "name": "BRA-HF-AMG-MARITIME227",
+    "acs": [
+      {
+        "name": "Vivian Pereira",
+        "fte": 8.0
+      },
+      {
+        "name": "Fabiana Souza Rocha",
+        "fte": 8.0
+      },
+      {
+        "name": "Valeria Aguiar",
+        "fte": 4.0
+      }
+    ],
+    "total_fte": 20.0,
+    "ingresos_hoy": 10,
+    "backlog": {
+      "En gestión AC": 147,
+      "Visita Cumplida": 4,
+      "Ingreso del Paciente": 2,
+      "Visita Programada": 9,
+      "Contactado por el Investigador": 5
+    },
+    "total_backlog": 167,
+    "gina_hoy": 0,
+    "gina_total": 0
+  },
+  {
+    "name": "CHL-CVOT-AMG-MARITIME196",
+    "acs": [
+      {
+        "name": "Jaime Del Río",
+        "fte": 6.0
+      },
+      {
+        "name": "Maria Amelia Gonzalez",
+        "fte": 6.0
+      }
+    ],
+    "total_fte": 12.0,
+    "ingresos_hoy": 8,
+    "backlog": {
+      "Ingreso del Paciente": 28,
+      "En gestión AC": 29
+    },
+    "total_backlog": 57,
+    "gina_hoy": 11,
+    "gina_total": 103
+  },
+  {
+    "name": "CHL-HF-AMG-MARITIME227",
+    "acs": [
+      {
+        "name": "Marcela Sajama",
+        "fte": 8.0
+      },
+      {
+        "name": "Ana Perotti",
+        "fte": 6.0
+      },
+      {
+        "name": "Juliana Vilchez Pereira",
+        "fte": 6.0
+      },
+      {
+        "name": "Lucia Aquilano",
+        "fte": 4.0
+      }
+    ],
+    "total_fte": 24.0,
+    "ingresos_hoy": 1,
+    "backlog": {
+      "Ingreso del Paciente": 157,
+      "En gestión AC": 97,
+      "Visita Programada": 1,
+      "Contactado por el Investigador": 1,
+      "Visita Cumplida": 1,
+      "Derivado": 3
+    },
+    "total_backlog": 260,
+    "gina_hoy": 1,
+    "gina_total": 26
+  },
+  {
+    "name": "LATAM-CROHN-MSD-ST",
+    "acs": [
+      {
+        "name": "Germán Keller",
+        "fte": 8.0
+      },
+      {
+        "name": "Maria Julia Gonzalez Palau",
+        "fte": 8.0
+      },
+      {
+        "name": "Juan Mur",
+        "fte": 8.0
+      },
+      {
+        "name": "Maria Isabel Barberena",
+        "fte": 6.0
+      },
+      {
+        "name": "Lucia Aquilano",
+        "fte": 4.0
+      },
+      {
+        "name": "Yadira Velasco",
+        "fte": 4.0
+      }
+    ],
+    "total_fte": 38.0,
+    "ingresos_hoy": 1,
+    "backlog": {
+      "Ingreso del Paciente": 1,
+      "Visita Cumplida": 5,
+      "Consentimiento Firmado": 3,
+      "Contactado por el Investigador": 4,
+      "En gestión AC": 15,
+      "Derivado": 5,
+      "Visita Programada": 2
+    },
+    "total_backlog": 35,
+    "gina_hoy": 0,
+    "gina_total": 0
+  },
+  {
+    "name": "MEX-HF-AMG-MARITIME227",
+    "acs": [
+      {
+        "name": "Danaee Quintero",
+        "fte": 8.0
+      }
+    ],
+    "total_fte": 8.0,
+    "ingresos_hoy": 4,
+    "backlog": {
+      "En gestión AC": 36,
+      "Consentimiento Firmado": 1,
+      "Derivado": 2,
+      "Visita Programada": 1
+    },
+    "total_backlog": 40,
+    "gina_hoy": 0,
+    "gina_total": 0
+  },
+  {
+    "name": "MEX-SARD-ILD-BI-1305-0046",
+    "acs": [
+      {
+        "name": "Aldana  Herrera",
+        "fte": 8.0
+      },
+      {
+        "name": "Erasmo  Moncada",
+        "fte": 8.0
+      }
+    ],
+    "total_fte": 16.0,
+    "ingresos_hoy": 7,
+    "backlog": {
+      "Ingreso del Paciente": 35,
+      "En gestión AC": 136,
+      "Consentimiento Firmado": 4,
+      "Contactado por el Investigador": 5,
+      "Derivado": 10,
+      "Visita Cumplida": 6,
+      "Visita Programada": 1,
+      "Pre Derivado": 1
+    },
+    "total_backlog": 198,
+    "gina_hoy": 0,
+    "gina_total": 0
+  }
+];
+window.LAST_UPDATED = "27/05/2026 15:47";
