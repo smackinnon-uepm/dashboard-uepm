16dbfaf86c472e32ea873729af55a83fdc825692
smackinnon-uepm <smackinnon@unensayoparami.org>
2026-05-28 16:34:33 +0100

    Dashboard update 2026-05-28

--- data.js
+++ data.js
@@ -1,33 +1,33 @@
-// Generado: 27/05/2026 15:47 hora Argentina
+// Generado: 28/05/2026 12:34 hora Argentina
 window.DASHBOARD_DATA = [
   {
     "name": "ARG-ALZ-KARUNA-BMS-ADEPT4",
     "acs": [
       {
         "name": "Ariadna Aixa Yael  Castillo",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Eliangelys  Camejo",
-        "fte": 6.0
+        "fte": 6
       },
       {
         "name": "Norberto Selasco",
-        "fte": 4.0
+        "fte": 4
       }
     ],
-    "total_fte": 18.0,
+    "total_fte": 18,
     "ingresos_hoy": 5,
     "backlog": {
-      "Ingreso del Paciente": 2,
-      "En gestión AC": 76,
-      "Derivado": 8,
+      "Ingreso del Paciente": 7,
+      "En gestión AC": 75,
       "Consentimiento Firmado": 4,
-      "Visita Programada": 4,
       "Visita Cumplida": 1,
-      "Pre Derivado": 1
+      "Visita Programada": 5,
+      "Contactado por el Investigador": 2,
+      "Derivado": 6
     },
-    "total_backlog": 96,
+    "total_backlog": 100,
     "gina_hoy": 0,
     "gina_total": 0
   },
@@ -36,83 +36,85 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Cinthia Nogues",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Milagros  Tani",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Roxana Lorena Diaz",
-        "fte": 8.0
+        "fte": 8
       }
     ],
-    "total_fte": 24.0,
-    "ingresos_hoy": 13,
+    "total_fte": 24,
+    "ingresos_hoy": 2,
     "backlog": {
-      "En gestión AC": 300,
-      "Ingreso del Paciente": 91,
-      "Contactado por el Investigador": 12,
+      "En gestión AC": 289,
+      "Ingreso del Paciente": 95,
       "Visita Programada": 12,
+      "Contactado por el Investigador": 12,
+      "Derivado": 14,
       "Consentimiento Firmado": 5,
-      "Derivado": 13,
       "Visita Cumplida": 2
     },
-    "total_backlog": 435,
-    "gina_hoy": 22,
-    "gina_total": 935
+    "total_backlog": 429,
+    "gina_hoy": 3,
+    "gina_total": 948
   },
   {
     "name": "ARG-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Antonella Mellado",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Giannina La pietra",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Agustina Tanca",
-        "fte": 6.0
+        "fte": 6
       }
     ],
-    "total_fte": 22.0,
-    "ingresos_hoy": 4,
+    "total_fte": 22,
+    "ingresos_hoy": 2,
     "backlog": {
-      "Ingreso del Paciente": 303,
-      "En gestión AC": 100,
+      "Ingreso del Paciente": 294,
+      "En gestión AC": 98,
       "Visita Programada": 1,
-      "Visita Cumplida": 1,
-      "Derivado": 2
+      "Derivado": 2,
+      "Visita Cumplida": 1
     },
-    "total_backlog": 407,
-    "gina_hoy": 9,
-    "gina_total": 170
+    "total_backlog": 396,
+    "gina_hoy": 2,
+    "gina_total": 176
   },
   {
     "name": "BRA-ALZ-KARUNA-BMS-ADEPT4",
     "acs": [
       {
         "name": "Maria Fernanda  Barbosa",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Alessandra D'Amico",
-        "fte": 8.0
+        "fte": 8
       }
     ],
-    "total_fte": 16.0,
-    "ingresos_hoy": 8,
+    "total_fte": 16,
+    "ingresos_hoy": 4,
     "backlog": {
-      "En gestión AC": 66,
+      "Ingreso del Paciente": 13,
+      "En gestión AC": 61,
       "Visita Cumplida": 4,
       "Contactado por el Investigador": 6,
+      "Pre Derivado": 1,
       "Consentimiento Firmado": 3,
       "Visita Programada": 1
     },
-    "total_backlog": 80,
+    "total_backlog": 89,
     "gina_hoy": 0,
     "gina_total": 0
   },
@@ -121,27 +123,27 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Vivian Pereira",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Fabiana Souza Rocha",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Valeria Aguiar",
-        "fte": 4.0
+        "fte": 4
       }
     ],
-    "total_fte": 20.0,
-    "ingresos_hoy": 10,
+    "total_fte": 20,
+    "ingresos_hoy": 6,
     "backlog": {
-      "En gestión AC": 147,
+      "Ingreso del Paciente": 20,
+      "En gestión AC": 133,
       "Visita Cumplida": 4,
-      "Ingreso del Paciente": 2,
-      "Visita Programada": 9,
-      "Contactado por el Investigador": 5
+      "Visita Programada": 11,
+      "Contactado por el Investigador": 3
     },
-    "total_backlog": 167,
+    "total_backlog": 171,
     "gina_hoy": 0,
     "gina_total": 0
   },
@@ -150,55 +152,55 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Jaime Del Río",
-        "fte": 6.0
+        "fte": 6
       },
       {
         "name": "Maria Amelia Gonzalez",
-        "fte": 6.0
+        "fte": 6
       }
     ],
-    "total_fte": 12.0,
-    "ingresos_hoy": 8,
+    "total_fte": 12,
+    "ingresos_hoy": 1,
     "backlog": {
-      "Ingreso del Paciente": 28,
-      "En gestión AC": 29
+      "Ingreso del Paciente": 24,
+      "En gestión AC": 34
     },
-    "total_backlog": 57,
-    "gina_hoy": 11,
-    "gina_total": 103
+    "total_backlog": 58,
+    "gina_hoy": 2,
+    "gina_total": 112
   },
   {
     "name": "CHL-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Marcela Sajama",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Ana Perotti",
-        "fte": 6.0
+        "fte": 6
       },
       {
         "name": "Juliana Vilchez Pereira",
-        "fte": 6.0
+        "fte": 6
       },
       {
         "name": "Lucia Aquilano",
-        "fte": 4.0
+        "fte": 4
       }
     ],
-    "total_fte": 24.0,
-    "ingresos_hoy": 1,
+    "total_fte": 24,
+    "ingresos_hoy": 0,
     "backlog": {
       "Ingreso del Paciente": 157,
-      "En gestión AC": 97,
-      "Visita Programada": 1,
-      "Contactado por el Investigador": 1,
+      "En gestión AC": 94,
+      "Derivado": 3,
       "Visita Cumplida": 1,
-      "Derivado": 3
+      "Visita Programada": 1,
+      "Contactado por el Investigador": 1
     },
-    "total_backlog": 260,
-    "gina_hoy": 1,
+    "total_backlog": 257,
+    "gina_hoy": 0,
     "gina_total": 26
   },
   {
@@ -206,41 +208,41 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Germán Keller",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Maria Julia Gonzalez Palau",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Juan Mur",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Maria Isabel Barberena",
-        "fte": 6.0
+        "fte": 6
       },
       {
         "name": "Lucia Aquilano",
-        "fte": 4.0
+        "fte": 4
       },
       {
         "name": "Yadira Velasco",
-        "fte": 4.0
+        "fte": 4
       }
     ],
-    "total_fte": 38.0,
+    "total_fte": 38,
     "ingresos_hoy": 1,
     "backlog": {
-      "Ingreso del Paciente": 1,
+      "Ingreso del Paciente": 4,
       "Visita Cumplida": 5,
-      "Consentimiento Firmado": 3,
       "Contactado por el Investigador": 4,
       "En gestión AC": 15,
       "Derivado": 5,
+      "Consentimiento Firmado": 3,
       "Visita Programada": 2
     },
-    "total_backlog": 35,
+    "total_backlog": 38,
     "gina_hoy": 0,
     "gina_total": 0
   },
@@ -249,18 +251,19 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Danaee Quintero",
-        "fte": 8.0
+        "fte": 8
       }
     ],
-    "total_fte": 8.0,
-    "ingresos_hoy": 4,
+    "total_fte": 8,
+    "ingresos_hoy": 1,
     "backlog": {
-      "En gestión AC": 36,
+      "Ingreso del Paciente": 1,
+      "En gestión AC": 35,
       "Consentimiento Firmado": 1,
-      "Derivado": 2,
-      "Visita Programada": 1
+      "Visita Programada": 1,
+      "Derivado": 3
     },
-    "total_backlog": 40,
+    "total_backlog": 41,
     "gina_hoy": 0,
     "gina_total": 0
   },
@@ -269,28 +272,28 @@ window.DASHBOARD_DATA = [
     "acs": [
       {
         "name": "Aldana  Herrera",
-        "fte": 8.0
+        "fte": 8
       },
       {
         "name": "Erasmo  Moncada",
-        "fte": 8.0
+        "fte": 8
       }
     ],
-    "total_fte": 16.0,
-    "ingresos_hoy": 7,
+    "total_fte": 16,
+    "ingresos_hoy": 5,
     "backlog": {
-      "Ingreso del Paciente": 35,
-      "En gestión AC": 136,
+      "Ingreso del Paciente": 31,
+      "Derivado": 11,
+      "En gestión AC": 122,
       "Consentimiento Firmado": 4,
-      "Contactado por el Investigador": 5,
-      "Derivado": 10,
       "Visita Cumplida": 6,
-      "Visita Programada": 1,
-      "Pre Derivado": 1
+      "Pre Derivado": 1,
+      "Contactado por el Investigador": 5,
+      "Visita Programada": 1
     },
-    "total_backlog": 198,
+    "total_backlog": 181,
     "gina_hoy": 0,
     "gina_total": 0
   }
 ];
-window.LAST_UPDATED = "27/05/2026 15:47";
+window.LAST_UPDATED = "28/05/2026 12:34";
