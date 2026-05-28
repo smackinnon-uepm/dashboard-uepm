03f6ff665f44b08df099c55ae0a88e6e701a0459
smackinnon-uepm <smackinnon@unensayoparami.org>
2026-05-28 18:59:32 +0100

    Dashboard update 2026-05-28 14:59

--- data.js
+++ data.js
@@ -1,299 +1,828 @@
-// Generado: 28/05/2026 13:27 hora Argentina
+// Generado: 28/05/2026 14:59 hora Argentina
 window.DASHBOARD_DATA = [
   {
     "name": "ARG-ALZ-KARUNA-BMS-ADEPT4",
     "acs": [
       {
         "name": "Ariadna Aixa Yael  Castillo",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Eliangelys  Camejo",
-        "fte": 6
+        "fte": 6.0
       },
       {
         "name": "Norberto Selasco",
-        "fte": 4
+        "fte": 4.0
       }
     ],
-    "total_fte": 18,
-    "ingresos_hoy": 5,
+    "total_fte": 18.0,
+    "ingresos_hoy": 7,
     "backlog": {
-      "Ingreso del Paciente": 7,
-      "En gestión AC": 75,
-      "Consentimiento Firmado": 4,
-      "Visita Cumplida": 1,
+      "Ingreso del Paciente": 2,
+      "En gestión AC": 73,
       "Visita Programada": 5,
+      "Consentimiento Firmado": 4,
       "Contactado por el Investigador": 2,
+      "Visita Cumplida": 1,
       "Derivado": 6
     },
-    "total_backlog": 100,
+    "total_backlog": 93,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        85,
+        93,
+        95,
+        101,
+        92,
+        91,
+        89,
+        90,
+        81,
+        87,
+        104,
+        104,
+        98,
+        95,
+        93
+      ],
+      "ingresos": [
+        5,
+        12,
+        6,
+        6,
+        4,
+        8,
+        9,
+        10,
+        8,
+        8,
+        17,
+        3,
+        7,
+        6,
+        7
+      ]
+    }
   },
   {
     "name": "ARG-CVOT-AMG-MARITIME196",
     "acs": [
       {
         "name": "Cinthia Nogues",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Milagros  Tani",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Roxana Lorena Diaz",
-        "fte": 8
+        "fte": 8.0
       }
     ],
-    "total_fte": 24,
-    "ingresos_hoy": 1,
+    "total_fte": 24.0,
+    "ingresos_hoy": 4,
     "backlog": {
-      "En gestión AC": 289,
+      "En gestión AC": 284,
       "Ingreso del Paciente": 95,
-      "Visita Programada": 12,
-      "Contactado por el Investigador": 12,
-      "Derivado": 14,
       "Consentimiento Firmado": 5,
+      "Derivado": 9,
+      "Visita Programada": 10,
+      "Contactado por el Investigador": 17,
       "Visita Cumplida": 2
     },
-    "total_backlog": 429,
-    "gina_hoy": 3,
-    "gina_total": 948
+    "total_backlog": 422,
+    "gina_hoy": 26,
+    "gina_total": 969,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        310,
+        299,
+        317,
+        349,
+        373,
+        370,
+        374,
+        373,
+        376,
+        399,
+        420,
+        427,
+        433,
+        425,
+        422
+      ],
+      "ingresos": [
+        8,
+        5,
+        8,
+        10,
+        14,
+        7,
+        12,
+        15,
+        20,
+        20,
+        16,
+        18,
+        20,
+        12,
+        4
+      ]
+    }
   },
   {
     "name": "ARG-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Antonella Mellado",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Giannina La pietra",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Agustina Tanca",
-        "fte": 6
+        "fte": 6.0
       }
     ],
-    "total_fte": 22,
-    "ingresos_hoy": 1,
+    "total_fte": 22.0,
+    "ingresos_hoy": 2,
     "backlog": {
-      "Ingreso del Paciente": 294,
-      "En gestión AC": 98,
+      "Ingreso del Paciente": 282,
+      "En gestión AC": 101,
       "Visita Programada": 1,
-      "Derivado": 2,
-      "Visita Cumplida": 1
+      "Visita Cumplida": 1,
+      "Derivado": 2
     },
-    "total_backlog": 396,
-    "gina_hoy": 2,
-    "gina_total": 176
+    "total_backlog": 387,
+    "gina_hoy": 6,
+    "gina_total": 178,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        388,
+        377,
+        385,
+        390,
+        387,
+        384,
+        384,
+        389,
+        383,
+        390,
+        394,
+        403,
+        404,
+        391,
+        387
+      ],
+      "ingresos": [
+        0,
+        0,
+        2,
+        1,
+        6,
+        3,
+        2,
+        6,
+        2,
+        5,
+        2,
+        5,
+        4,
+        4,
+        2
+      ]
+    }
   },
   {
     "name": "BRA-ALZ-KARUNA-BMS-ADEPT4",
     "acs": [
       {
         "name": "Maria Fernanda  Barbosa",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Alessandra D'Amico",
-        "fte": 8
+        "fte": 8.0
       }
     ],
-    "total_fte": 16,
-    "ingresos_hoy": 4,
+    "total_fte": 16.0,
+    "ingresos_hoy": 10,
     "backlog": {
-      "Ingreso del Paciente": 13,
-      "En gestión AC": 61,
       "Visita Cumplida": 4,
-      "Contactado por el Investigador": 6,
-      "Pre Derivado": 1,
+      "En gestión AC": 62,
       "Consentimiento Firmado": 3,
+      "Contactado por el Investigador": 6,
+      "Derivado": 1,
+      "Ingreso del Paciente": 3,
       "Visita Programada": 1
     },
-    "total_backlog": 89,
+    "total_backlog": 80,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        75,
+        70,
+        90,
+        103,
+        106,
+        100,
+        92,
+        80,
+        66,
+        83,
+        100,
+        93,
+        83,
+        83,
+        80
+      ],
+      "ingresos": [
+        17,
+        13,
+        20,
+        13,
+        19,
+        12,
+        14,
+        12,
+        11,
+        17,
+        18,
+        11,
+        14,
+        15,
+        10
+      ]
+    }
   },
   {
     "name": "BRA-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Vivian Pereira",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Fabiana Souza Rocha",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Valeria Aguiar",
-        "fte": 4
+        "fte": 4.0
       }
     ],
-    "total_fte": 20,
-    "ingresos_hoy": 6,
+    "total_fte": 20.0,
+    "ingresos_hoy": 15,
     "backlog": {
-      "Ingreso del Paciente": 20,
-      "En gestión AC": 133,
-      "Visita Cumplida": 4,
-      "Visita Programada": 11,
+      "Ingreso del Paciente": 1,
+      "En gestión AC": 128,
+      "Visita Cumplida": 5,
+      "Visita Programada": 10,
       "Contactado por el Investigador": 3
     },
-    "total_backlog": 171,
+    "total_backlog": 147,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        192,
+        182,
+        212,
+        252,
+        240,
+        210,
+        193,
+        189,
+        161,
+        190,
+        220,
+        189,
+        180,
+        161,
+        147
+      ],
+      "ingresos": [
+        14,
+        37,
+        30,
+        40,
+        29,
+        24,
+        20,
+        29,
+        27,
+        29,
+        30,
+        23,
+        20,
+        19,
+        15
+      ]
+    }
   },
   {
     "name": "CHL-CVOT-AMG-MARITIME196",
     "acs": [
       {
         "name": "Jaime Del Río",
-        "fte": 6
+        "fte": 6.0
       },
       {
         "name": "Maria Amelia Gonzalez",
-        "fte": 6
+        "fte": 6.0
       }
     ],
-    "total_fte": 12,
-    "ingresos_hoy": 1,
+    "total_fte": 12.0,
+    "ingresos_hoy": 3,
     "backlog": {
-      "Ingreso del Paciente": 24,
-      "En gestión AC": 34
+      "Ingreso del Paciente": 23,
+      "En gestión AC": 33
     },
-    "total_backlog": 58,
-    "gina_hoy": 2,
-    "gina_total": 112
+    "total_backlog": 56,
+    "gina_hoy": 6,
+    "gina_total": 116,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        3,
+        4,
+        4,
+        4,
+        5,
+        5,
+        5,
+        5,
+        15,
+        31,
+        39,
+        49,
+        52,
+        57,
+        56
+      ],
+      "ingresos": [
+        0,
+        1,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        4,
+        9,
+        9,
+        9,
+        6,
+        4,
+        3
+      ]
+    }
   },
   {
     "name": "CHL-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Marcela Sajama",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Ana Perotti",
-        "fte": 6
+        "fte": 6.0
       },
       {
         "name": "Juliana Vilchez Pereira",
-        "fte": 6
+        "fte": 6.0
       },
       {
         "name": "Lucia Aquilano",
-        "fte": 4
+        "fte": 4.0
       }
     ],
-    "total_fte": 24,
-    "ingresos_hoy": 0,
+    "total_fte": 24.0,
+    "ingresos_hoy": 1,
     "backlog": {
-      "Ingreso del Paciente": 157,
-      "En gestión AC": 94,
-      "Derivado": 3,
-      "Visita Cumplida": 1,
+      "Ingreso del Paciente": 156,
+      "En gestión AC": 80,
       "Visita Programada": 1,
-      "Contactado por el Investigador": 1
+      "Derivado": 2,
+      "Visita Cumplida": 1,
+      "Contactado por el Investigador": 2
     },
-    "total_backlog": 257,
-    "gina_hoy": 0,
-    "gina_total": 26
+    "total_backlog": 242,
+    "gina_hoy": 1,
+    "gina_total": 26,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        373,
+        357,
+        366,
+        380,
+        369,
+        353,
+        331,
+        311,
+        300,
+        305,
+        305,
+        302,
+        277,
+        259,
+        242
+      ],
+      "ingresos": [
+        13,
+        6,
+        9,
+        14,
+        11,
+        8,
+        7,
+        2,
+        12,
+        3,
+        0,
+        4,
+        2,
+        1,
+        1
+      ]
+    }
   },
   {
     "name": "LATAM-CROHN-MSD-ST",
     "acs": [
       {
         "name": "Germán Keller",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Maria Julia Gonzalez Palau",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Juan Mur",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Maria Isabel Barberena",
-        "fte": 6
+        "fte": 6.0
       },
       {
         "name": "Lucia Aquilano",
-        "fte": 4
+        "fte": 4.0
       },
       {
         "name": "Yadira Velasco",
-        "fte": 4
+        "fte": 4.0
       }
     ],
-    "total_fte": 38,
-    "ingresos_hoy": 1,
+    "total_fte": 38.0,
+    "ingresos_hoy": 2,
     "backlog": {
-      "Ingreso del Paciente": 4,
-      "Visita Cumplida": 5,
+      "Ingreso del Paciente": 5,
+      "En gestión AC": 13,
       "Contactado por el Investigador": 4,
-      "En gestión AC": 15,
       "Derivado": 5,
+      "Visita Cumplida": 5,
       "Consentimiento Firmado": 3,
       "Visita Programada": 2
     },
-    "total_backlog": 38,
+    "total_backlog": 37,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        40,
+        39,
+        41,
+        42,
+        41,
+        41,
+        43,
+        42,
+        34,
+        34,
+        34,
+        35,
+        35,
+        37,
+        37
+      ],
+      "ingresos": [
+        2,
+        0,
+        2,
+        1,
+        0,
+        0,
+        3,
+        2,
+        0,
+        0,
+        0,
+        2,
+        1,
+        3,
+        2
+      ]
+    }
   },
   {
     "name": "MEX-HF-AMG-MARITIME227",
     "acs": [
       {
         "name": "Danaee Quintero",
-        "fte": 8
+        "fte": 8.0
       }
     ],
-    "total_fte": 8,
+    "total_fte": 8.0,
     "ingresos_hoy": 1,
     "backlog": {
-      "Ingreso del Paciente": 1,
       "En gestión AC": 35,
       "Consentimiento Firmado": 1,
-      "Visita Programada": 1,
-      "Derivado": 3
+      "Derivado": 3,
+      "Visita Programada": 1
     },
-    "total_backlog": 41,
+    "total_backlog": 40,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        41,
+        35,
+        39,
+        42,
+        45,
+        43,
+        43,
+        46,
+        36,
+        39,
+        41,
+        42,
+        38,
+        40,
+        40
+      ],
+      "ingresos": [
+        5,
+        0,
+        4,
+        3,
+        5,
+        3,
+        5,
+        6,
+        2,
+        3,
+        2,
+        2,
+        5,
+        4,
+        1
+      ]
+    }
   },
   {
     "name": "MEX-SARD-ILD-BI-1305-0046",
     "acs": [
       {
         "name": "Aldana  Herrera",
-        "fte": 8
+        "fte": 8.0
       },
       {
         "name": "Erasmo  Moncada",
-        "fte": 8
+        "fte": 8.0
       }
     ],
-    "total_fte": 16,
-    "ingresos_hoy": 5,
+    "total_fte": 16.0,
+    "ingresos_hoy": 8,
     "backlog": {
-      "Ingreso del Paciente": 31,
-      "Derivado": 11,
-      "En gestión AC": 122,
+      "Ingreso del Paciente": 27,
       "Consentimiento Firmado": 4,
-      "Visita Cumplida": 6,
+      "Derivado": 10,
+      "En gestión AC": 122,
       "Pre Derivado": 1,
-      "Contactado por el Investigador": 5,
+      "Contactado por el Investigador": 6,
+      "Visita Cumplida": 6,
       "Visita Programada": 1
     },
-    "total_backlog": 181,
+    "total_backlog": 177,
     "gina_hoy": 0,
-    "gina_total": 0
+    "gina_total": 0,
+    "trend": {
+      "dates": [
+        "14/05",
+        "15/05",
+        "16/05",
+        "17/05",
+        "18/05",
+        "19/05",
+        "20/05",
+        "21/05",
+        "22/05",
+        "23/05",
+        "24/05",
+        "25/05",
+        "26/05",
+        "27/05",
+        "28/05"
+      ],
+      "backlogs": [
+        208,
+        181,
+        200,
+        222,
+        231,
+        219,
+        206,
+        187,
+        169,
+        183,
+        196,
+        201,
+        194,
+        176,
+        177
+      ],
+      "ingresos": [
+        18,
+        10,
+        19,
+        22,
+        17,
+        10,
+        4,
+        0,
+        4,
+        14,
+        13,
+        8,
+        5,
+        7,
+        8
+      ]
+    }
   }
 ];
-window.LAST_UPDATED = "28/05/2026 13:27";
+window.LAST_UPDATED = "28/05/2026 14:59";
